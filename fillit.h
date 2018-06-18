6dbf47724e27a55784b6514bc010646ac0eacc42
Guillaume FRANCO <gfranco@e1r6p14.42.fr>
2018-06-18 17:27:25 +0200

    modif structure + commentaires

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: gfranco <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/18 12:00:06 by gfranco           #+#    #+#             */
-/*   Updated: 2018/06/18 12:03:39 by gfranco          ###   ########.fr       */
+/*   Updated: 2018/06/18 17:27:05 by gfranco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,20 @@
 # include <unistd.h>
 # include <fcntl.h>
 
-void		ft_error(int argc);
+typedef struct	s_bloc //structure pour la position x et y d'un bloc
+{
+	int		x;
+	int		y;
+}				t_bloc;
+
+typedef struct	s_tetri// structure qui regroupe les 4 bloc d'un tetriminos
+{
+	t_bloc a;          //via t_tetri->a.x et a.y on peut donner les valeurs
+	t_bloc b;
+	t_bloc c;
+	t_bloc d;
+}				t_tetri;
+
+void			ft_error(int argc);
 
 #endif
