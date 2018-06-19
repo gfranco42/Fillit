ebbc57eacb1ebe45d2d865ddab3dbaf7020e6b4c
Guillaume FRANCO <gfranco@e1r6p14.42.fr>
2018-06-19 15:44:57 +0200

    modifs du 19 06 2018

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: gfranco <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/18 12:00:06 by gfranco           #+#    #+#             */
-/*   Updated: 2018/06/18 17:27:05 by gfranco          ###   ########.fr       */
+/*   Updated: 2018/06/19 15:44:01 by gfranco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,23 @@
 
 typedef struct	s_bloc //structure pour la position x et y d'un bloc
 {
-	int		x;
-	int		y;
+	int			x;
+	int			y;
 }				t_bloc;
 
 typedef struct	s_tetri// structure qui regroupe les 4 bloc d'un tetriminos
 {
-	t_bloc a;          //via t_tetri->a.x et a.y on peut donner les valeurs
-	t_bloc b;
-	t_bloc c;
-	t_bloc d;
+	t_bloc		a;     //via t_tetri->a.x et a.y on peut donner les valeurs
+	t_bloc		b;
+	t_bloc		c;
+	t_bloc		d;
 }				t_tetri;
+
+typedef struct	s_stock
+{
+	t_tetri		tetri;
+	s_stock		*next;
+}				t_stock;
 
 void			ft_error(int argc);
 
--- ft_memtetri.c
+++ ft_memtetri.c
@@ -6,7 +6,7 @@
 /*   By: gfranco <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/18 12:10:05 by gfranco           #+#    #+#             */
-/*   Updated: 2018/06/18 17:22:29 by gfranco          ###   ########.fr       */
+/*   Updated: 2018/06/19 15:44:36 by gfranco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,26 @@
  *
  * oui c'est moche mais bon, bref, passons.*/
 
-t_tetri		ft_memtetri(char *file)
+t_tetri		ft_memtetri(char *file, int i)
 {
 	t_tetri		*tetriminos;
+	int			count;
 
-	while (file[walk] != '#') //tu cherche le premier "bloc" d'un tetriminos
-		walk++;
+	count = 0;
+	while (file[i] != '#') //tu cherche le premier "bloc" d'un tetriminos
+		i++;
 	tetriminos->a.x = i % 5; //tu enregistre laposition du premier bloc 'a'.
 	tetriminos->a.y = i / 5;
-	while (file[walk] != '#')//tu recommences
-		walk++;
+	while (file[i] != '#')//tu recommences
+		i++;
 	tetriminos->b.x = i % 5;//etc
 	tetriminos->b.y = i / 5;//etc..
-	while (file[walk] != '#')
-		walk++;
+	while (file[i] != '#')
+		i++;
 	tetriminos->c.x = i % 5;//bref ensuite j'ai pas rajouter les conditions
 	tetriminos->c.y = i / 5;// d'arret style "si ya pas 4 blocs etc etc"
-	while (file[walk] != '#')//mais on verra apres.
-		walk++;
+	while (file[i] != '#')//mais on verra apres.
+		i++;
 	tetriminos->d.x = i % 5;
 	tetriminos->d.y = i / 5;
 }
