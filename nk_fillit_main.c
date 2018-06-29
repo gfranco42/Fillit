6e1309c0d3af1020b5fb8c2e852252a57cd2168b
Narjisse KORCHI <nakorchi@e2r12p1.42.fr>
2018-06-29 19:24:09 +0200

    NAKORCHI = supposedly functionnal main able to open, read, copy and split into a tab the parameter file

--- nk_fillit_main.c
+++ nk_fillit_main.c
@@ -6,23 +6,30 @@
 /*   By: nakorchi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/19 15:56:50 by nakorchi          #+#    #+#             */
-/*   Updated: 2018/06/29 15:18:54 by gfranco          ###   ########.fr       */
+/*   Updated: 2018/06/29 19:08:49 by nakorchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int		main()
+int		main(int ac, char **av)
 {
-	char	BUFF[546];
+	//	char	BUFF[546];
 	char	*stock;
 	char	**g;
 	int		x;
 
-	stock = NULL;
-	while ((x = read(0, BUFF, 546)) != 0)
-		stock = ft_strjoin(stock, BUFF);
+	stock = "";
+	if (ac != 2)
+		ft_putstr("usage: ./fillit target_file");
+	if ((x = open (av[1], O_RDONLY)) != 0)
+	{
+		if ((x = read(x, av[1], 546)) != 0)
+		{
+			stock = ft_strjoin(stock, av[1]);
+		}
+	}
 	g = ft_strsplit(stock, '\n');
-	check_file(**g);
-
+	return(0);
 }
+
