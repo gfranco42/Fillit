14d92e338f1e649812a7b96975a244500faab4fb
camah17 <camah17@hotmail.com>
2018-09-04 16:01:28 +0200

    ft_overlap fn added, but have compilation errors. still in prog.

---
+++ src/ft_overlap.c
@@ -0,0 +1,28 @@
+int     ft_overlap(int nbr)
+{
+    int i; //placeholder for outcome
+    int n; //tetris index no.
+    int j; //
+    char    **str;
+    int     ***array;
+    
+    i = 0;
+//  j = 0;
+    n = 0;
+    array = ft_stocktetri(str, nbr);
+//  block = ft_stocktetri(*str[n], nbr);
+    while (n < nbr - 1)
+    {
+        j = 0;
+        while (j < 4)
+        {
+            if ((str[n][0][j] == str[n + 1][0][j]) && (str[n][1][j] == str[n + 1][1][j]))
+                i = 1;
+            j++;
+        }
+        n++;
+    }
+//  printf("overlap exists if this is 1: %d\n", ft_overlap(block, nbr));
+    printf("overlap exists if this is 1: %d\n", ft_overlap(nbr));
+    return (i);
+}
