7aa9c9abd8ce7de8f4581f0aa809c91d4c899755
Guillaume FRANCO <38046550+gfranco42@users.noreply.github.com>
2018-09-07 13:42:10 +0200

    Create alpha.c

---
+++ src/alpha.c
@@ -0,0 +1,23 @@
+char    **ft_alpha(char **map, int **array)
+{
+    int i;
+    int j;
+    int c;
+    int d;
+    
+    i = 0;
+    j = 0;
+    c = 0;
+    d = 'A' + c
+    while (i < 4)
+    {
+        while (j < 4)
+        {
+            map[(*array)[0][i][(*array)[1][i]]] = ft_putchar(d);
+            j++;
+        }
+        c++;
+        i++;
+    }
+    return (0);
+}
