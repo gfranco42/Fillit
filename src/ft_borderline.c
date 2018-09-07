152a5f970064bedc29981fff5f3dfe660185adcf
camah17 <camah17@hotmail.com>
2018-09-07 10:19:13 +0200

    uploading borderline fn as a separate file

---
+++ src/ft_borderline.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_borderline.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cabdul-h <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/07 10:08:02 by cabdul-h          #+#    #+#             */
+/*   Updated: 2018/09/07 10:16:42 by cabdul-h         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/fillit.h"
+
+int     ft_borderline(char **map, int **array)
+{   
+    size_t  ms;
+    int     i;
+    int     j;
+    
+    ms = ft_strlen(map[0]) - 1;
+    i = 0;
+    j = 0;
+    while (i < 4)
+    {   
+        if (array[1][i] >= (int)ms)
+            return (1);
+        if (array[0][j] >= (int)ms)
+            return (2);
+        i++;
+        j++;
+    }   
+    return (0);
+}
